be6ec525f555b391976d098023f7ac93f1bb0257
jamieleegtb <cemilegtaman@gmail.com>
2018-02-04 20:56:38 -0600

    Pixed Art code updated.

--- PixelArtProject/designs.js
+++ PixelArtProject/designs.js
@@ -1,10 +1,34 @@
-// Select color input
-// Select size input
+$(document).ready(function(){
+    
+    // Color and Canvas Variables
+    var color = $('#colorPicker').attr('value');
+    var gridCanvas = $('#pixelCanvas');
 
-// When size is submitted by the user, call makeGrid()
+    // Grid Size Variables
+    var height = $('inputHeight').val();
+    var width = $('inputWidth').val();
 
-function makeGrid() {
 
-// Your code goes here!
+    // TODO: Creates rows and columns inside the #pixel_canvas table.
+    function makeGrid() {
+        gridCanvas.children().remove();
+        for(var i = 1; i <= height; i++){ //row
+            gridCanvas.append('<tr></tr>');
+            for(var j=1; j <= width; j++){ // column
+                $('tr:nth-child(' + i + ')').append('<td></td>'); 
+            }
+        }
+    }
 
-}
+    // TODO: On click adds selected background color to columns.
+    $(canvas).on('click', 'td', function(event){
+        $(this).css('background', color);
+    });
+
+    // TODO: When sizes submitted creates grids.
+    $('#sizePicker').on(submit, function(event){
+        event.preventDefault();
+        makeGrid();
+    });
+
+});
