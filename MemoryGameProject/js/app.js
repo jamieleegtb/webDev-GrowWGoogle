5713c47a60cd587311e3743eaef9a22d1013cb9a
jamieleegtb <cemilegtaman@gmail.com>
2018-05-27 02:45:06 -0500

    HTML - JS - CSS files edited
    HTML: List of cards deleted / Timer added
    CSS: Responsive
    JS: Cards listed & shuffled / Reser function added / Animation classes
    added / If condition added for stars

--- MemoryGameProject/js/app.js
+++ MemoryGameProject/js/app.js
@@ -1,7 +1,19 @@
 /*
  * Create a list that holds all of your cards
  */
+const cards = ['fa-diamond', 'fa-diamond',
+             'fa-paper-plane-o', 'fa-paper-plane-o',
+             'fa-anchor', 'fa-anchor',
+             'fa-bolt', 'fa-bolt',
+             'fa-cube', 'fa-cube',
+             'fa-leaf', 'fa-leaf',
+             'fa-bicycle', 'fa-bicycle',
+             'fa-bomb', 'fa-bomb'
+            ];
 
+function generateCard(card){
+  return `<li class="card animated" data-card="${card}"> <i class="fa ${card}"> </i> </li>`;
+}
 
 /*
  * Display the cards on the page
@@ -36,3 +48,87 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+function initGame(){
+  const deck = document.querySelector('.deck');
+  const cardHTML = shuffle(cards).map(function(card){
+    return generateCard(card);
+  });
+  deck.innerHTML = cardHTML.join('');
+}
+
+//Initialize game
+initGame();
+
+const allCards = document.querySelectorAll('.card');
+//Empty array for open cards
+let openCards = [];
+//Timer
+let seconds = 0;
+let minutes = 0;
+//Move counter
+let moves = 0;
+const moveCounter = document.querySelector('.moves');
+moveCounter.innerText = moves;
+//stars
+const stars = document.querySelectorAll('.fa-star');
+
+$(document).ready(function(){
+  //TODO: Reset page.
+  $('.restart').click(function(){
+      location.reload();
+  });
+});
+
+allCards.forEach(function(card){
+    //With eventlistener 'click' function added to all cards
+    card.addEventListener('click', function(e){
+
+      //Checking if the card has 'open' & 'show' class
+      if ((!card.classList.contains('open') || !card.classList.contains('show')) && !card.classList.contains('match')){
+        openCards.push(card);
+        //Adds 'open' & 'show' classes to cards after click
+        card.classList.add('open', 'show', 'flipInY');
+        card.classList.remove('shake')
+
+        //Removing stars
+        if (moves === 16) {
+          stars[0].remove();
+        } else if (moves === 32) {
+          stars[1].remove();
+        } else if (moves === 48) {
+          stars[2].remove();
+        }
+
+        if (openCards.length == 2){
+          //Check if cards match
+          if (openCards[0].dataset.card == openCards[1].dataset.card){
+              openCards[0].classList.add('match');
+              openCards[0].classList.add('open');
+              openCards[0].classList.add('show');
+              openCards[0].classList.add('flipInY');
+
+              openCards[1].classList.add('match');
+              openCards[1].classList.add('open');
+              openCards[1].classList.add('show');
+              openCards[1].classList.add('flipInY');
+
+              openCards = [];
+          } else {
+              //'open' & 'show' classes will be deleted from cards after 1 second
+              //If cards don't match - go away
+              setTimeout(function(){
+                openCards.forEach(function(card){
+                  card.classList.remove('open', 'show', 'flipInY');
+                  card.classList.add('shake');
+                });
+                //openCards array empitied out after two cards opened
+                openCards = [];
+              }, 1000);
+            }
+            moves += 1;
+            moveCounter.innerText = moves;
+        }
+      }
+    });
+});
